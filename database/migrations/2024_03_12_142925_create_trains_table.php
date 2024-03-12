3c4ce66661a2a005374ac972202ddbdadcc2e172
lelz99 <emanuelesimonetti99@gmail.com>
2024-03-12 17:58:03 +0100

    create table train

---
+++ database/migrations/2024_03_12_142925_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('rail_company', 100);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->datetime('departure_time')->nullable();
+            $table->datetime('arrival_time')->nullable();
+            $table->char('train_code', 6);
+            $table->integer('carriage');
+            $table->boolean('on_time')->default(1);
+            $table->boolean('canceled')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
 @section('table-trains')
-    cioa
+
 @endsection
